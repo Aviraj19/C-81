7a9ba6908017796a5833a931376e399d3a159e66
Aviraj19 <73528868+Aviraj19@users.noreply.github.com>
2021-02-04 16:16:13 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -7,5 +7,9 @@ ctx.arc(100,200,50,0,2*Math.PI);
 ctx.stroke();
 canvas.addEventListener("mousedown",my_mousedown);
 function my_mousedown(e) {
-    
+    mouse_x=e.clientX-canvas.offsetLeft;
+    mouse_y=e.clientY-canvas.offsetTop;
+    ctx.beginPath();
+    ctx.arc(mouse_x,mouse_y,50,0,2*Math.PI);
+    ctx.stroke();
 }
